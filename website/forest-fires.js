5b21391670d38307ecfafc8c33cc0d8094425548
Jonathan Kicillof <jkicillof@manas.com.ar>
2020-08-14 19:17:01 -0300

    Full responsive

--- website/forest-fires.js
+++ website/forest-fires.js
@@ -1,19 +1,12 @@
 function shuffle(array) {
 	var currentIndex = array.length, temporaryValue, randomIndex;
-  
-	// While there remain elements to shuffle...
 	while (0 !== currentIndex) {
-  
-	  // Pick a remaining element...
 	  randomIndex = Math.floor(Math.random() * currentIndex);
 	  currentIndex -= 1;
-  
-	  // And swap it with the current element.
 	  temporaryValue = array[currentIndex];
 	  array[currentIndex] = array[randomIndex];
 	  array[randomIndex] = temporaryValue;
 	}
-  
 	return array;
 }
 
@@ -30,46 +23,40 @@ function handleResize() {
 		picture = {
 			width: 2800,
 			height: 1800,
-			fire: {
-				x: 850,
-				y: 850
-			}
+		},
+		fire = {
+			x: 660,
+			y: 680,
+			width: 350,
+			height: 350
 		},
 		background = {
 			width: window.innerWidth,
 			height: window.innerHeight,
-			position: {},
-			size: {}
 		},
 		frame = {
+			x: document.querySelector('#mask').offsetLeft,
+			y: document.querySelector('#mask').offsetTop,
 			width: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-width')),
-			height: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-height')),
-			x: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-x')),
-			y: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-y')),
-			center: {
-				x: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-x')) + parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-width')) / 2,
-				y: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-y')) + parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-height')) / 2
-			},
-			position: {}
+			height: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-boundary-frame-height'))
 		},
 		scale = Math.max(
-			frame.center.x / picture.fire.x,
-			frame.center.y / picture.fire.y,
-			(background.width - frame.center.x) / (picture.width - picture.fire.x),
-			(background.height - frame.center.y) / (picture.height - picture.fire.y)
+			background.width / picture.width,
+			background.height / picture.height,
+			frame.x / fire.x,
+			frame.y / fire.y,
+			(frame.x + frame.width / 2) / (fire.x + fire.width / 2),
+			(frame.y + frame.height / 2) / (fire.y + fire.height / 2),
+			(background.width - frame.width - frame.x) / (picture.width - fire.width - fire.x),
+			(background.height - frame.height - frame.y) / (picture.height - fire.height - fire.y),
+			(background.width - frame.width / 2 - frame.x) / (picture.width - fire.width / 2 - fire.x),
+			(background.height - frame.height / 2 - frame.y) / (picture.height - fire.height / 2 - fire.y)
 		)
-
-	background.size.width = Math.round(picture.width * scale)
-	background.size.height = Math.round(picture.height * scale)
-	background.position.x = Math.round(frame.center.x - picture.fire.x * scale)
-	background.position.y = Math.round(frame.center.y - picture.fire.y * scale)
-
-	frame.position.x = Math.round(background.position.x - frame.center.x + frame.width / 2)
-	frame.position.y = Math.round(background.position.y - frame.center.y + frame.height / 2)
-
-	style.setProperty('--fire-boundary-size', `${background.size.width}px ${background.size.height}px`)
-	style.setProperty('--fire-boundary-background-position', `${background.position.x}px ${background.position.y}px`)
-	style.setProperty('--fire-boundary-frame-position', `${frame.position.x}px ${frame.position.y}px`)
+	console.log(frame, scale)
+	document.querySelector('#value').textContent = scale.toFixed(3)
+	style.setProperty('--fire-boundary-size', `${picture.width * scale}px ${picture.height * scale}px`)
+	style.setProperty('--fire-boundary-background-position', `${(frame.x + frame.width / 2) - (fire.x + fire.width / 2) * scale}px ${(frame.y + frame.height / 2) - (fire.y + fire.height / 2) * scale}px`)
+	style.setProperty('--fire-boundary-frame-position', `${frame.width / 2 - (fire.x + fire.width / 2) * scale}px ${frame.height / 2 - (fire.y + fire.height / 2) * scale}px`)
 }
 
 window.onload = handleLoad
